8af8f1c01d6cde146f0c0d675b3a4390cb9cc4e2
Suvadip Mondal <suvadipm07@gmail.com>
2026-09-01 13:23:49 +0530

    delete and chat clear api added

--- backend/chat_actions.py
+++ backend/chat_actions.py
@@ -8,15 +8,13 @@ messages_collection
 
 from auth import get_current_user
 
-#==========================================
 #ROUTER
-#==========================================
 
 chat_actions_router = APIRouter()
 
-#==========================================
+
 #CLEAR ALL MESSAGES
-#==========================================
+
 
 @chat_actions_router.delete(
 "/messages/{chat_id}/clear"
@@ -31,142 +29,130 @@ current_user_id: str = Depends(
 
 ):
 
-# ======================================
-# CHECK CHAT EXISTS
-# ======================================
-
-chat = chats_collection.find_one({
+    # CHECK CHAT EXISTS
+    chat = chats_collection.find_one({
 
     "_id": ObjectId(chat_id)
 
-})
+    })
 
 
-if not chat:
+    if not chat:
 
-    raise HTTPException(
+        raise HTTPException(
 
-        status_code=404,
+            status_code=404,
 
-        detail="Chat not found"
+            detail="Chat not found"
 
-    )
-
-
-# ======================================
-# CHECK USER BELONGS TO CHAT
-# ======================================
-
-if current_user_id not in chat["participants"]:
-
-    raise HTTPException(
-
-        status_code=403,
-
-        detail="You are not a participant of this chat"
-
-    )
+        )
 
 
-# ======================================
-# DELETE ALL MESSAGES
-# ======================================
+    # CHECK USER BELONGS TO CHAT
 
-result = messages_collection.delete_many({
+    if current_user_id not in chat["participants"]:
 
-    "chat_id": chat_id
+        raise HTTPException(
 
-})
+            status_code=403,
+
+            detail="You are not a participant of this chat"
+
+        )
 
 
-return {
+    # DELETE ALL MESSAGES
 
-    "message":
-        "All messages cleared",
 
-    "deleted_count":
-        result.deleted_count
+    result = messages_collection.delete_many({
 
-}
-==========================================
-DELETE COMPLETE CHAT
-==========================================
+        "chat_id": chat_id
+
+    })
+
+
+    return {
+
+        "message":
+            "All messages cleared",
+
+        "deleted_count":
+            result.deleted_count
+
+    }
+
+#DELETE COMPLETE CHAT
+
 
 @chat_actions_router.delete(
 "/chat/{chat_id}"
 )
 def delete_chat(
 
-chat_id: str,
+    chat_id: str,
 
-current_user_id: str = Depends(
+    current_user_id: str = Depends(
     get_current_user
-)
+    )
 
-):
+    ):
 
-# ======================================
-# CHECK CHAT EXISTS
-# ======================================
 
-chat = chats_collection.find_one({
+    # CHECK CHAT EXISTS
+
+
+    chat = chats_collection.find_one({
 
     "_id": ObjectId(chat_id)
 
-})
-
-
-if not chat:
-
-    raise HTTPException(
-
-        status_code=404,
-
-        detail="Chat not found"
-
+    }
     )
 
 
-# ======================================
-# CHECK USER BELONGS TO CHAT
-# ======================================
+    if not chat:
 
-if current_user_id not in chat["participants"]:
+        raise HTTPException(
 
-    raise HTTPException(
+            status_code=404,
 
-        status_code=403,
+            detail="Chat not found"
 
-        detail="You are not a participant of this chat"
-
-    )
+        )
 
 
-# ======================================
-# DELETE ALL CHAT MESSAGES
-# ======================================
+    # CHECK USER BELONGS TO CHAT
 
-messages_collection.delete_many({
+    if current_user_id not in chat["participants"]:
 
-    "chat_id": chat_id
+        raise HTTPException(
 
-})
+            status_code=403,
 
+            detail="You are not a participant of this chat"
 
-# ======================================
-# DELETE CHAT
-# ======================================
+        )
 
-chats_collection.delete_one({
-
-    "_id": ObjectId(chat_id)
-
-})
+    # DELETE ALL CHAT MESSAGES
 
 
-return {
+    messages_collection.delete_many({
 
-    "message":
-        "Chat deleted successfully"
+        "chat_id": chat_id
 
-}
+    })
+
+
+
+    # DELETE CHAT
+
+
+    chats_collection.delete_one({
+
+        "_id": ObjectId(chat_id)
+
+    })
+
+    return {
+
+        "message":"Chat deleted successfully"
+    }
--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,8 @@ app.include_router(chat_actions_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=[ "http://localhost:5173",
+        "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
