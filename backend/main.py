1416d56b2b111ab1153c2d15849f8bcc4db14653
Suvadip Mondal <suvadipm07@gmail.com>
2026-09-11 23:18:19 +0530

    Updated whole project

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
    allow_origins = [
     "https://YOUR-FRONTEND.onrender.com",
     "http://localhost:5173"
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
