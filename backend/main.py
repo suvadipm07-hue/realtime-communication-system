0a4d16ae97763720f8cc69773660dba0b8694217
Suvadip Mondal <suvadipm07@gmail.com>
2026-09-11 23:12:27 +0530

    Updated whole project

--- backend/main.py
+++ backend/main.py
@@ -32,8 +32,10 @@ app.include_router(chat_actions_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173",
-        "http://127.0.0.1:5173"],
+   allow_origins = [
+    "https://YOUR-FRONTEND.onrender.com",
+    "http://localhost:5173"
+    ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
